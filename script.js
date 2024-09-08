32f074343dcf0b1600302a1ea74fcc3a6b4f7847
CodeForgeNet <codeforgenet@icloud.com>
2024-09-09 00:12:15 +0530

    js file edit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("working")
+console.log("working");
+
+document.querySelector(".btn").addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log("button clicked")
+    console.log(username.value, password.value)
+    let passwords = localStorage.getItem("passwords")
+    console.log(passwords)
+    if (passwords == null) {
+        let json = []
+        json.push({ username: username.value, password: password.value })
+        alert("Password Saved")
+        localStorage.setItem("passwords", JSON.stringify(json))
+    } else {
+        let json = JSON.parse(localStorage.getItem("passwords"))
+        console.log(json)
+        json.push({ username: username.value, password: password.value })
+        alert("Password Saved")
+        localStorage.setItem("passwords", JSON.stringify(json))
+    }
+
+})
