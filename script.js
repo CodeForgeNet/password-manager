52120106dc458d9fa1588c8044ab822439741507
CodeForgeNet <codeforgenet@icloud.com>
2025-01-22 17:20:33 +0530

    another commit

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 // Logic to fill the table.
 
-let tb = document.querySelector("table")
-let data = localStorage.getItem("passwords")
-if (data == null) {
-    tb.innerHTML = "No data to show"
-} else {
-    let arr = JSON.parse(data);
-    let str = ""
-    for (let index = 0; index < arr.length; index++) {
-        const element = arr[index];
+const showPasswword = () => {
+
+    let tb = document.querySelector("table")
+    let data = localStorage.getItem("passwords")
+    if (data == null) {
+        tb.innerHTML = "No data to show"
+    } else {
+        tb.innerHTML = `<tr>
+                <th>Websites</th>
+                <th>Username</th>
+                <th>Password</th>
+                <th>Delete</th>
+            </tr>`
+        let arr = JSON.parse(data);
+        let str = ""
+        for (let index = 0; index < arr.length; index++) {
+            const element = arr[index];
 
 
-        str += `<tr>
+            str += `<tr>
                 <td>${element.website}</td>
                 <td>${element.username}</td>
                 <td>${element.password}</td>
                 <td>${"Delete"}</td>
             </tr>`
 
-        tb.innerHTML += str
+            tb.innerHTML += str
+        }
     }
 }
 
 console.log("working");
+
+showPasswword();
 
 document.querySelector(".btn").addEventListener("click", (e) => {
     e.preventDefault()
@@ -32,15 +43,16 @@ document.querySelector(".btn").addEventListener("click", (e) => {
     console.log(passwords)
     if (passwords == null) {
         let json = []
-        json.push({ username: username.value, password: password.value })
+        json.push({website:website.value, username: username.value, password: password.value })
         alert("Password Saved")
         localStorage.setItem("passwords", JSON.stringify(json))
     } else {
         let json = JSON.parse(localStorage.getItem("passwords"))
         console.log(json)
-        json.push({ username: username.value, password: password.value })
+        json.push({website:website.value, username: username.value, password: password.value })
         alert("Password Saved")
         localStorage.setItem("passwords", JSON.stringify(json))
     }
+    showPasswword();
 
 })
